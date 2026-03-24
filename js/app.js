b474aa528917f6a45f906db3f46eea9805246003
Sanki_coder <aaryandhirhe@gmail.com>
2026-03-24 12:20:16 +0530

    Update food images with new URLs

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@ let data = [
   {
     name: "Paneer Chilli",
     price: 200,
-    image: "https://source.unsplash.com/400x300/?paneer"
+    image: "https://images.unsplash.com/photo-1604908176997-125f25cc6f3d"
   },
   {
     name: "Veg Manchurian",
     price: 170,
-    image: "https://source.unsplash.com/400x300/?manchurian"
+    image: "https://images.unsplash.com/photo-1625944525533-473f1c3d54c3"
   },
   {
     name: "Hakka Noodles",
     price: 150,
-    image: "https://source.unsplash.com/400x300/?noodles"
+    image: "https://images.unsplash.com/photo-1612929633738-8fe44f7ec841"
   }
 ];
