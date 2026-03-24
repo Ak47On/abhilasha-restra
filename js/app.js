9535260a0562eae6342c6290952d1e815dc7eeeb
Sanki_coder <aaryandhirhe@gmail.com>
2026-03-24 12:16:55 +0530

    Remove Veg Thali from menu data
    Removed Veg Thali from the menu data.

--- js/app.js
+++ js/app.js
@@ -13,22 +13,5 @@ let data = [
     name: "Hakka Noodles",
     price: 150,
     image: "https://source.unsplash.com/400x300/?noodles"
-  },
-  {
-    name: "Veg Thali",
-    price: 180,
-    image: "https://source.unsplash.com/400x300/?indian,thali"
   }
 ];
-
-let container = document.getElementById("menu-container");
-
-data.forEach(item => {
-  container.innerHTML += `
-    <div class="card">
-      <img src="${item.image}">
-      <h3>${item.name}</h3>
-      <p>₹${item.price}</p>
-    </div>
-  `;
-});
